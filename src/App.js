c6c16682392a26c8ffa11df7812ae27365eff8a8
gessica <gessicamohr18@gmail.com>
2020-12-23 11:20:21 -0800

    implement timelineevent component function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
